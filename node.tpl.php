bff2ec9642b4afaeb1d7375ff7a7fd519b5d10f7
Jason Moore <sk33lz@gmail.com>
2011-02-27 02:06:17 -0500

    major code cleanup

--- node.tpl.php
+++ node.tpl.php
@@ -2,11 +2,11 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
-<?php print $picture ?>
+  <?php print $picture ?>
 
-<?php if ($page == 0): ?>
-  <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-<?php endif; ?>
+  <?php if ($page == 0): ?>
+    <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+  <?php endif; ?>
 
   <?php if ($submitted): ?>
     <span class="submitted"><?php print $submitted; ?></span>
@@ -27,5 +27,4 @@
       <div class="links"><?php print $links; ?></div>
     <?php endif; ?>
   </div>
-
 </div>
